d725837e963b54059c9780e6db8b157cfacb829e
Link <link.lin@udngroup.com>
2022-09-15 15:39:59 +0800

    feat: setup env file

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+// routes
 import image from './routes/image'
+
+// set env
+const currentEnv = process.env.NODE_ENV?.trim() ?? 'development'
+dotenv.config({ path: `./.env.${currentEnv}` })
 
 const app = express()
 
 app.use('/image', image)
 
-app.listen(3000, () => {
-  console.log('server running')
+app.listen(process.env.PORT, () => {
+  console.log(`server running at: http://localhost:${process.env.PORT as string}`)
 })
