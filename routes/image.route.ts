81f9ffbd115b71072d44c18f9bb7450726caaff7
Link <link.lin@udngroup.com>
2023-10-11 14:57:54 +0800

    modify docs

--- routes/image.route.ts
+++ routes/image.route.ts
@@ -19,7 +19,7 @@ const router = Router()
  *        - Image
  *      parameters:
  *        - name: url
- *          description: image origin url
+ *          description: image origin url in udn.com domain
  *          in: query
  *          schema:
  *            type: string
